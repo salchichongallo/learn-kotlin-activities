977581a2ffb22832ab72e528f30cf88c51a7f212
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-10-20 21:17:05 -0500

    feat: navigate to word detail activity

--- app/src/main/java/com/example/kotlinpocintentsandactivities/WordDetailActivity.kt
+++ app/src/main/java/com/example/kotlinpocintentsandactivities/WordDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.example.kotlinpocintentsandactivities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,16 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinpocintentsandactivities.ui.theme.KotlinPoCIntentsAndActivitiesTheme
 
-class MainActivity : ComponentActivity() {
+class WordDetailActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,38 +22,14 @@ class MainActivity : ComponentActivity() {
             KotlinPoCIntentsAndActivitiesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
+                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
                         verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Greeting(
-                            name = "Android", modifier = Modifier.padding(innerPadding)
-                        )
-                        Button(onClick = {
-                            Intent(applicationContext, WordDetailActivity::class.java).also { startActivity(it) }
-                        }) {
-                            Text("Open word detail")
-                        }
+                        Text(text = "Hello, word detail activity!")
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinPoCIntentsAndActivitiesTheme {
-        Greeting("Android")
     }
 }
